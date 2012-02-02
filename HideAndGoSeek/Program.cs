d53a1e307e7ac27e5d462ec731d5d91172506267
Michael Petruzzello <Tedybear3@aol.com>
2012-02-01 22:15:05 -0500

    Begun building the house.

--- HideAndGoSeek/Program.cs
+++ HideAndGoSeek/Program.cs
@@ -7,11 +7,46 @@ namespace HideAndGoSeek
 {
     class Program
     {
+        Location myCurrentLocation;
+        static Room hallway;
+        static Room stairwayToSecondFloor;
+        static Room stairwayToBasement;
+        static Room stairwayToAttic;
+        static RoomWithHidingPlace bathroom;
+        static RoomWithHidingPlace kitchen;
+        static RoomWithHidingPlace livingroom;
+        static RoomWithHidingPlace masterBedroom;
+        static RoomWithHidingPlace basement;
+        static RoomWithHidingPlace attic;
+        static RoomWithHidingPlace kidsBedroom;
+        static RoomWithHidingPlace guestBedroom;
+        static Outside frontyard;
+        static OutsideWithHidingPlace garage;
+        static OutsideWithHidingPlace backyard;
+        static Outside garden;
+
         static void Main(string[] args)
         {
             string userName;
 
             Introduction(out userName);
+            CreateLocations();
+        }
+
+        /// <summary>
+        /// Instantiates all the locations and sets their exits.
+        /// </summary>
+        private static void CreateLocations()
+        {
+            attic = new RoomWithHidingPlace("Attic", "a lot of old, dusty boxes.", "behind the boxes.");
+            stairwayToAttic = new Room("Stairway to the Attic", "rickety old steps.");
+            hallway = new Room("Hallway", "an ancient, antique rug.");
+            bathroom = new RoomWithHidingPlace("Bathroom", "a gold-plated toilet.", "in the shower.");
+            masterBedroom = new RoomWithHidingPlace("Master Bedroom", "a king-sized bed.", "in the walk-in closet.");
+            kidsBedroom = new RoomWithHidingPlace("Kids' Bedroom", "a colorful room with a bunkbed.", "in the toy chest.");
+            guestBedroom = new RoomWithHidingPlace("Guest Bedroom", "nothing unique here.", "under the bed.");
+            stairwayToSecondFloor = new Room("Stairway to the 2nd Floor", "a crystal chandelier.");
+
         }
 
         /// <summary>
@@ -39,7 +74,7 @@ namespace HideAndGoSeek
                 Console.WriteLine("\nThe object of the game is  to find your opponent. When the game begins you need to count to 10 while your opponent hides. Then you must" +
                     " search throughout the   house in order to find him. Type \"goto\" plus the name of the location you want  to go to. I.E. goto kitchen . If you would " +
                     "like to check a hiding spot, type    \"check\".");
-                Console.WriteLine("Are you ready? ");
+                Console.Write("\nAre you ready? ");
                 Console.Read();
             }
             Console.Clear();
